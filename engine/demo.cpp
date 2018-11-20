cc03710a7293b061ac3da4298a75ebee593e0cdd
mHvNG <mhoving@hdms.nl>
2018-11-20 14:17:37 +0100

    first commit

---
+++ engine/demo.cpp
@@ -0,0 +1,16 @@
+
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+
+// Include GLFW
+#include <glfw3.h>
+
+// Include GLM
+#include <glm/glm.hpp>
+
+#include <opengine/shader.h>
+
+int main( void ) {
+    return 0;
+}
