b873af041500ba051fa5d47d33a6a638e872555a
mHvNG <mhoving@hdms.nl>
2019-03-18 19:00:28 +0100

    Added an init function and some properties for text

--- src/UI/UIText.cpp
+++ src/UI/UIText.cpp
@@ -1,10 +1,27 @@
 #include "UIText.h"
 
-UIText::UIText(const char *fontPath) {
+UIText::UIText(const char *fontPath, float _scale) {
     this->setFont(fontPath);
+    this->scale = _scale;
     
+    this->content = "No content";
+
+    this->init();
 }
 
 void UIText::setFont(const char *fontP) {
     font = fontP;
+}
+
+void UIText::init() {
+    // create text quad
+	glGenVertexArrays(1, &_VAO);
+	glGenBuffers(1, &_VBO);
+	glBindVertexArray(_VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, _VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), 0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 }
--- src/UI/UIText.h
+++ src/UI/UIText.h
@@ -1,15 +1,32 @@
 #ifndef UITEXT_H
 #define UITEXT_H
 
+//include glad
+#include <glad.h>
+//include streams
+#include <iostream>
 // include freetype
 #include <ft2build.h>
 #include FT_FREETYPE_H
+//include glm
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class UIText {
 public:
-    UIText(const char *fontPath);
+    UIText(const char *fontPath, float _scale);
     virtual ~UIText();
     void setFont(const char *fontP);
+    void init();
+    //Properties
+    float scale;
+    glm::vec3 color;
+    std::string content;
+    glm::vec3 pos;
+
+    unsigned int _VAO;
+	unsigned int _VBO; 
 private:
     const char* font;
 };
