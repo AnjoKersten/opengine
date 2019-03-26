d684e61c7efc47ab8710da7116bbab201cf0ef72
mHvNG <mhoving@hdms.nl>
2019-03-26 13:40:07 +0100

    Actor class added

---
+++ src/Renderer/actor.h
@@ -0,0 +1,18 @@
+#ifndef ACTOR_H
+#define ACTOR_H
+
+#include <vector>
+
+class Actor {
+public:
+	Actor();
+	virtual ~Actor();
+	void childActor(Actor* actor) { actors.push_back(actor); };
+	void removeChildActor(Actor* actor);
+	std::vector<Actor*> getActors() { return actors; };
+	int getActorCount() { return actors.size(); };
+private:
+	std::vector<Actor*> actors;
+};
+
+#endif
