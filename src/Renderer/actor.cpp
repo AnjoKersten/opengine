9e9e694902a93151149f969af62b37e887217a67
mHvNG <mhoving@hdms.nl>
2019-03-27 11:06:50 +0100

    Input implemented in Actor and added the removeChildActor function

--- src/Renderer/actor.cpp
+++ src/Renderer/actor.cpp
@@ -4,10 +4,21 @@ Actor::Actor() {
 	transform.position = glm::vec3(0.0f, 0.0f, 0.0f);
 	transform.scale = glm::vec3(0.0f, 0.0f, 0.0f);
 	transform.rotation = glm::vec3(0.0f, 0.0f, 0.0f);
+
+	active = true;
 }
 
 Actor::~Actor() {
 	
+}
+
+void Actor::removeChildActor(Actor* actor) {
+	for (int i = 0; i < actors.size(); i++) {
+		if (actors[i] == actor) {
+			actors.erase(actors.begin() + i);
+			break;
+		}	
+	}
 }
 
 
--- src/Renderer/actor.h
+++ src/Renderer/actor.h
@@ -3,18 +3,22 @@
 
 #include <vector>
 #include "src/Maths/transform.h"
+#include "src/Core/input.h" 
 
 class Actor {
 public:
+	bool active;
 	Actor();
 	virtual ~Actor();
+	Input* input() { return _input; }
 	void childActor(Actor* actor) { actors.push_back(actor); };
 	void removeChildActor(Actor* actor);
 	std::vector<Actor*> getActors() { return actors; };
 	int getActorCount() { return actors.size(); };
 private:
 	std::vector<Actor*> actors;
-
+	Input* _input;
+	Transform transform;
 };
 
 #endif
