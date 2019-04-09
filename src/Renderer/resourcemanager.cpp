352699e01a4d135f6c8d0841d3f7033dedf95050
mHvNG <mhoving@hdms.nl>
2019-04-09 11:26:21 +0200

    Made an getTexture for the resource manager

--- src/Renderer/resourcemanager.cpp
+++ src/Renderer/resourcemanager.cpp
@@ -1,6 +1,8 @@
 #include "resourcemanager.h"
 
 ResourceManager::ResourceManager() {
+	std::cout << "_____________________________________"
+	std::cout << "New Resource Manager created!" << std::endl;	
 
 }
 
@@ -10,6 +12,12 @@ ResourceManager::~ResourceManager() {
 
 GLuint ResourceManager::getTexture(const char* path) {
 	if (textures[path] != NULL) {
-
+		return textures[path];
+	} else {
+		Texture* tex = new Texture();
+		tex.loadTexture(path);
+		textures[path] = tex;
+		std::cout << "return new Texture: " << path << std::endl;
+		return tex;
 	}
 }
